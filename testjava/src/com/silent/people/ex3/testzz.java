108f21f9cb037d168c5103022d0519628fa449bb
BeomJoon <stars6141@naver.com>
2016-11-22 20:18:38 +0900

    2016 11 22

---
+++ testjava/src/com/silent/people/ex3/testzz.java
@@ -0,0 +1,70 @@
+package com.silent.people.ex3;
+
+public class testzz {
+	public static void main(String[] args){
+		System.out.println("age1");
+		/*
+		int i = 0;
+		while(i<10){
+		    System.out.println("coding everybody"+(i+1)*2);
+		    i++;*/
+	
+	 /*public static void main(String[] args) {
+		 String id="star";
+		 String id2="stars";
+		 String pass = "1111";
+	     String phone = "01011112222";
+				 if(id.equals("stars") && pass.equals("1111s")||id2.equals("stars")){
+					 System.out.println("성공");}
+				 
+				 else if(id.equals("star") && pass.equals("1111")){
+						 System.out.println("성공");
+					 }
+
+				 
+				 else{System.out.println("실패");
+				 }
+				 
+				 int result = 1 + 2;
+				 result = result * 2;         
+				 System.out.println(result);*/
+		    /*byte salary = (byte)128L;
+	        byte salary1 = (byte)-129;
+	        byte salary2 = (byte)21000000000L;
+	        System.out.println(salary);
+	        System.out.println(salary1);
+	        System.out.println(salary2);*/
+	  /*      int i = 1;
+	        System.out.println(i---i);
+	        System.out.println(i---i);
+	        System.out.println(i+(++i));
+	        System.out.println(++i);
+	        
+		 int ii=0;
+		 System.out.println( ii---ii );
+		 System.out.println( ii---ii );
+		 System.out.println( ii---ii );
+		 System.out.println( ii---ii );
+		 System.out.println( ii+++ii );*/
+	        
+	  /*      System.out.println(i);     //1
+            System.out.println(++i); //2
+	        System.out.println(++i); //3
+	        System.out.println(++i); //4
+	        System.out.println(i++); //4
+	        System.out.println(++i); //6
+	        System.out.println(i++); //6
+	        System.out.println(i++); //7
+	        System.out.println(i++); //8
+	        System.out.println(++i); //10
+	        System.out.println(++i); //11
+	        System.out.println(i++); //11
+	        System.out.println(i++); //12
+	        System.out.println(++i); //14
+	        System.out.println(++i); //15
+	        System.out.println(i++); //15
+	        System.out.println(i++); //16
+*/	        
+	        }
+	 }
+
